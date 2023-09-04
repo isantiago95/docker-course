e6e2f18a959676b97c8ce874a9e78bae16432c00
Israel Santiago <israel.santiago4359@gmail.com>
2023-09-04 15:39:24 -0700

    first commit

---
+++ terminal.bash
@@ -0,0 +1,26 @@
+dc run \
+-dp 3306:3306 \
+--name world-db \
+--env MARIADB_USER=example-user \
+--env MARIADB_PASSWORD=user-password \
+--env MARIADB_ROOT_PASSWORD=root-secret-password \
+--env MARIADB_DATABASE=world-db \
+--volume world-db:/var/lib/mysql \
+--network world-app \
+mariadb:jammy
+
+dc run \
+--name phpmyadmin \
+-d \
+-e PMA_ARBITRARY=1 \
+-p 8080:80 \
+--network world-app \
+phpmyadmin:5.2-apache
+
+dc run \
+--name nest-app \
+-w /app \
+-dp 80:3000 \
+-v "$(pwd)":/app \
+node:18.17.1-slim \
+sh -c "yarn install && yarn start:dev"
